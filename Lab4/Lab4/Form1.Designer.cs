ab3a045c09ca38c63d496afae5f385be22ab0b8d
Yerzhan Mukhanov <63681904+Yerlan31@users.noreply.github.com>
2021-01-18 16:57:36 +0600

    Add files via upload

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReadFile = new System.Windows.Forms.Button();
+            this.textBoxReadTime = new System.Windows.Forms.TextBox();
+            this.labelReadTime = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxWord = new System.Windows.Forms.TextBox();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.textBoxSearchTime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonReadFile
+            // 
+            this.buttonReadFile.Location = new System.Drawing.Point(661, 57);
+            this.buttonReadFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonReadFile.Name = "buttonReadFile";
+            this.buttonReadFile.Size = new System.Drawing.Size(109, 25);
+            this.buttonReadFile.TabIndex = 0;
+            this.buttonReadFile.Text = "Чтение файла";
+            this.buttonReadFile.UseVisualStyleBackColor = true;
+            this.buttonReadFile.Click += new System.EventHandler(this.buttonReadFile_Click);
+            // 
+            // textBoxReadTime
+            // 
+            this.textBoxReadTime.Location = new System.Drawing.Point(423, 58);
+            this.textBoxReadTime.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxReadTime.Name = "textBoxReadTime";
+            this.textBoxReadTime.Size = new System.Drawing.Size(195, 22);
+            this.textBoxReadTime.TabIndex = 2;
+            // 
+            // labelReadTime
+            // 
+            this.labelReadTime.AutoSize = true;
+            this.labelReadTime.Location = new System.Drawing.Point(297, 57);
+            this.labelReadTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelReadTime.Name = "labelReadTime";
+            this.labelReadTime.Size = new System.Drawing.Size(113, 17);
+            this.labelReadTime.TabIndex = 3;
+            this.labelReadTime.Text = "Выберите файл";
+            this.labelReadTime.Click += new System.EventHandler(this.labelReadTime_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(661, 138);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(109, 25);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите слово для поиска";
+            // 
+            // textBoxWord
+            // 
+            this.textBoxWord.Location = new System.Drawing.Point(423, 138);
+            this.textBoxWord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxWord.Name = "textBoxWord";
+            this.textBoxWord.Size = new System.Drawing.Size(195, 22);
+            this.textBoxWord.TabIndex = 6;
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.ItemHeight = 16;
+            this.listBoxResults.Location = new System.Drawing.Point(423, 267);
+            this.listBoxResults.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(195, 260);
+            this.listBoxResults.TabIndex = 7;
+            // 
+            // textBoxSearchTime
+            // 
+            this.textBoxSearchTime.Location = new System.Drawing.Point(423, 203);
+            this.textBoxSearchTime.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxSearchTime.Name = "textBoxSearchTime";
+            this.textBoxSearchTime.Size = new System.Drawing.Size(195, 22);
+            this.textBoxSearchTime.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 207);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Время поиска";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 267);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Найденные слова";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(480, 101);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Поиск слов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 571);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSearchTime);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.textBoxWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelReadTime);
+            this.Controls.Add(this.textBoxReadTime);
+            this.Controls.Add(this.buttonReadFile);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReadFile;
+        private System.Windows.Forms.TextBox textBoxReadTime;
+        private System.Windows.Forms.Label labelReadTime;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxWord;
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.TextBox textBoxSearchTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();
+        private void buttonReadFile_Click(object sender, EventArgs e)
+        {
+            //выбираем файл через стандартное диалоговое окно
+            OpenFileDialog txt = new OpenFileDialog();
+            //Фильтр на TXT файлы
+            txt.Filter = "Только текстовые файлы|*.txt";
+            //если пользователь не выбрал файл
+            if (txt.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+                return;
+            }
+            //разделители слов
+            char[] separators = { '?', '.', ',', '!', '*','/', ' ', '\t', '\n'};
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            //считывание текста из файла
+            string text = File.ReadAllText(txt.FileName);
+            //удаление ненужных символов в начале и конце текста
+            text = text.Trim(separators);
+            //разделение строки на слова
+            string[] textArray = text.Split(separators);
+            //Запись слов в список
+            foreach (string word in textArray)
+            {
+                if (!word.Contains("-"))
+                //проверка на неповторяющиеся слова
+                    if (!list.Contains(word))
+                        list.Add(word);
+            }
+            timer.Stop();
+            //запись даных из таймера
+            this.textBoxReadTime.Text = timer.Elapsed.ToString();
+            //this.listBoxFile.BeginUpdate();
+            //this.listBoxFile.Items.Clear();
+            //foreach (string word in list)
+            //{
+            //    this.listBoxFile.Items.Add(word);
+            //}
+            //this.listBoxFile.EndUpdate();
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            //запись слова и его очистка
+            string word = this.textBoxWord.Text.Trim();
+            //Подтягиваем буквы вверх, чтобы не было проблем с регистром
+            word = word.ToUpper();
+            //запуск листбокса
+            this.listBoxResults.BeginUpdate();
+            this.listBoxResults.Items.Clear();
+            Stopwatch timer = new Stopwatch();
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                //Проверка на случай отсутствия совпадений
+                bool NoMatches = true;
+                //запуск таймера
+                timer.Start();
+                foreach (string w in list)
+                {
+                    //идёт проверка слов в верхнем регистре
+                    if (w.ToUpper().Contains(word))
+                    {
+                        this.listBoxResults.Items.Add(w);
+                        NoMatches = false;
+                    }
+                }
+                //остановка таймера
+                timer.Stop();
+                //Если совпадений всё же не нашлось
+                if (NoMatches) this.listBoxResults.Items.Add("Нет сопадений");
+            }
+            else
+            {
+                MessageBox.Show("Необходимо отрыть файл и выбрать слово для поиска");
+            }
+            this.listBoxResults.EndUpdate();
+            //запись даных из таймера
+            this.textBoxSearchTime.Text = timer.Elapsed.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelReadTime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
